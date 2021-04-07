cf0283e2f0aa49fe848c75533eb78c561cca9a75
nsoto92 <nor.soto92@gmail.com>
2021-04-06 23:50:05 -0700

    mvp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,14 +3,28 @@ import axios from 'axios'
 import { UglyContext } from '../UglyContext'
 
 export default function Form() {
-    const { data, setData, } = useContext(UglyContext)
+    const { data } = useContext(UglyContext)
     const [form, setForm] = useState({})
+    const [edit, setEdit] = useState(false)
 
     console.log('Form Data: ', form)
 
     const postData = (e) => {
         e.preventDefault()
         axios.post('https://api.vschool.io/norbert/thing/', {
+            title: form.title,
+            description: form.description,
+            imgUrl: form.imgUrl
+        })
+            .then(res => console.log(res))
+            .then(() => {
+                window.location.reload()
+            })
+    }
+
+    const updateData = (e) => {
+        e.preventDefault()
+        axios.put(`https://api.vschool.io/norbert/thing/${edit}`, {
             title: form.title,
             description: form.description,
             imgUrl: form.imgUrl
@@ -35,6 +49,26 @@ export default function Form() {
                 <input type="text" name='imgUrl' onChange={e => setForm({ ...form, imgUrl: e.target.value })} />
                 <br />
                 <button type='submit'>Submit</button>
+                <br />
+                <label>Select thing to edit (optional): </label>
+                <select
+                    onChange={e => setEdit(e.target.value)}
+                >
+                    <option> </option>
+                    {data.map(d => {
+                        return (
+                            <option value={d._id}>{d.title}</option>
+                        )
+                    })}
+                </select>
+                <br />
+                <label>Edit an existing thing: </label>
+                <button
+                    disabled={!edit}
+                    onClick={updateData}
+                >
+                    Edit
+                </button>
             </form>
         </div>
     )
